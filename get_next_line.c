9193f2ec6d3140a738c64fb5ec43f23f3b639840
Amandine Cocoual <acocoual@f0r3s5.clusters.42paris.fr>
2025-06-24 14:29:08 +0200

    mise a lecrit idee

--- get_next_line.c
+++ get_next_line.c
@@ -3,19 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amandine <amandine@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acocoual <acocoual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 09:21:33 by amandine          #+#    #+#             */
-/*   Updated: 2025/06/24 09:33:41 by amandine         ###   ########.fr       */
+/*   Updated: 2025/06/24 14:28:27 by acocoual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	str = malloc(sizeof(char) * (ft_strlen((char *)s1) + ft_strlen((char *)s2)
+				+ 1));
+	if (!str)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
 char *get_next_line(int fd);
 {
+    int i;
+    size_t count;
+    size_t octet_buf;
+    static char *buffer;
+    char *tmp;
+    char *line;
+
+    i = 0;
+    count = 16;
+    line = malloc(sizeof(char) * 1)
+        if(!line)
+            return (NULL);
+    line[o] = '\0';
+    octet_buf = read(fd, (void)*buffer, count);
+    buffer[octet_buf] = '\0';
+    tmp = ft_strdup((const char)*buffer);
+    while (octet_buf > 0)
+        buffer[octet_buf--] = '\0';
+    buffer[octet_buf] = '\0';
+    while (tmp[i] != '\0')
+    {
+        if (tmp[i] == '\n')
+        {
+            i++;
+            while (tmp[i] != '\0')
+            {
+                buffer[octet_buf] = tmp[i];
+                tmp[i] = '\0';
+                i++;
+            } 
+            line = ft_strjoin((char const)*line, (char const)*tmp);
+            return (line);
+        }
+        i++;
+    }
+    line = ft_strjoin((char const)*line, (char const)*tmp);
     
 }
+
+// ssize_t read(int fd, void *buf, size_t count)
+// ---------------------
 // Écrire une fonction qui retourne 
 // une ligne lue depuis un descripteur de fichier
 // ---------------------
